adb2c75c21cf4513e102615bcf1faa6c6f8e98d4
Rouj <renatojour@gmail.com>
2018-03-21 13:20:40 +0100

    User profil
    Get loged-in user data on the user profil page

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,3 +4,14 @@ import './body.html';
 import './template/accommodation.html';
 
 import './accommodation.js';
+
+
+Template.affich_us.helpers({
+    'email': function(){
+        data = Meteor.user();
+        address = data && data.emails[0].verified;
+        if (!address){
+            return 'true'
+        }
+    }
+})
