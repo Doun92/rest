1f70dbe1c1ca9bca85bc0d162a5f1726f9e9e027
MicTs01 <32584062+MicTs01@users.noreply.github.com>
2018-03-29 15:05:26 +0200

    Login - incomplet
    mise en place des routes, du template login, register. Création d'un template (vide) pour l'inscription des travailleurs sociaux.
    
    ajout d'un style au message d'alerte.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,13 +4,20 @@ import './body.html';
 
 //import './template/loginbar.html';
 import './accommodation.js';
+import './template/register.html';
 
+import './template/login_host.html';
+import './template/main-layout.html';
+
+
+import './login_host.js';
+
+import './register.js';
 import './template/host_profil_template.html';
 import './host_profil.js';
-import './accounts.js'
 
 import './template/colltest_template.html';
 import './colltest.js';
 
-import './template/social_worker_profile.html';
-import './social_worker_profile.js';
+//import './template/social_worker_profile.html';
+//import './social_worker_profile.js';
