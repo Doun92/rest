7d3f9c6c0a0dd42a894ca26f12464e14dfca737e
MicTs01 <32584062+MicTs01@users.noreply.github.com>
2018-03-30 10:54:27 +0200

    Formulaire d'inscription - Squelette
    Réduction du formulaire d'inscription et modification des messages d'erreur afficher.

--- imports/ui/register.js
+++ imports/ui/register.js
@@ -20,8 +20,8 @@ Template.register.events({
                 return pwd.length >= 6 ? true : false;
             } else {
                 return swal({
-                    title: "Passwords don't match",
-                    text: "Please try again",
+                    title: "Les mots de passes ne correspondent pas",
+                    text: "Veuillez ressayer",
                     showConfirmButton: true,
                     type: "error"
                 });
@@ -40,7 +40,7 @@ Template.register.events({
                 if (error) {
                     return swal({
                     title: error.reason,
-                    text: "Please try again",
+                    text: "Ressayer",
                     showConfirmButton: true,
                     type: "error"
                 });
