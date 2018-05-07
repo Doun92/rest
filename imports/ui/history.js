e08cfbe7b4c5ce2802e141a34bf9e85c15d72bc1
aubrays <contact@aubrays.ch>
2018-05-07 11:52:24 +0200

    History.template (WIP)

--- imports/ui/history.js
+++ imports/ui/history.js
@@ -3,11 +3,13 @@ import { Template } from 'meteor/templating';
 import { HistoryLocation } from "../api/history-methods.js"
 
 Template.history.events({
-    '.click #reservate': function(){
+    'click #reservate': function(event,template){
         const reservationDate = Date.now();
         const socialWorker = Meteor.userId();
         const host = this.host_id;
         const place = this.place_id;
+
+        console.log("click on reservate");
 
         HistoryLocation.insert({
             user_id : socialWorker,
