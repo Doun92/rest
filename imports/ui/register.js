fa98f0aed2d2437f9ea52fef549be6f212225820
aubrays <contact@aubrays.ch>
2018-05-09 10:00:01 +0200

    Cleaning and commenting

--- imports/ui/register.js
+++ imports/ui/register.js
@@ -1,64 +1,6 @@
-// Template.register.events({
-//     'submit .registerForm': function(e, t) {
-//         e.preventDefault();
-//         // Retrieve the input field values
-//         var email = $('#email').val(),
-//             firstName = $('#firstname').val(),
-//             lastName = $('#lastname').val(),
-//             password = $('#password').val(),
-//             passwordAgain = $('#passwordAgain').val();
+// Creating a general account. If the field "institute" are empty, nothings is added. On contrary, if "institute" exists, the field "sw" is updated to true.
 
-//         // Trim Helper
-//         var trimInput = function(val) {
-//             return val.replace(/^\s*|\s*$/g, "");
-//         }
-//         var email = trimInput(email);
-
-//         // Check password is at least 6 chars long
-//         var isValidPassword = function(pwd, pwd2) {
-//             if (pwd === pwd2) {
-//                 return pwd.length >= 6 ? true : false;
-//             } else {
-//                 return swal({
-//                     title: "Les mots de passe ne correspondent pas",
-//                     text: "Veuillez réessayer",
-//                     showConfirmButton: true,
-//                     type: "error"
-//                 });
-//             }
-//         }
-
-
-//         // If validation passes, supply the appropriate fields to the
-//         // Meteor.loginWithPassword() function.
-        
-//         if (isValidPassword(password, passwordAgain)) { 
-//             Accounts.createUser({
-//                 email: email,
-//                 firstname: firstName,
-//                 lastname: lastName,
-//                 password: password,
-//                 sw : false
-//             }, function(error) {
-//                 if (error) {
-//                     return swal({
-//                     title: error.reason,
-//                     text: "Ressayer",
-//                     showConfirmButton: true,
-//                     type: "error"
-//                 });
-//                 } else {
-//                     FlowRouter.go('/profil_utilisateur');
-//                 }
-//             });
-//         }
-
-//         return false;
-//     }
-// });
-
-// Création d'un compte général. Si le champ "institute" est vide, il n'est pas ajouté. Aussi si l'institute existe, update du champ "sw" en true
-// Ainsi une création unifiée des comptes ce qui simplifiera l'ajout ultérieur de compte Association
+// With this process, we can add later an Account for organization easier.
 
 Template.super_register_form.events({
     'submit .registerForm': function(e, t) {
