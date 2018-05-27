b51e202d3e13ca8d401c3911a3baa7ea2929e495
Rouj <renatojour@gmail.com>
2018-05-27 10:34:56 +0200

    comment + clean

--- imports/ui/calendar.js
+++ imports/ui/calendar.js
@@ -10,7 +10,7 @@ Template.calendar_template.onCreated(function(){
     this.subscribe('userData');
     this.subscribe('accommodations');
     
-    //global variables    
+    //init global var   
     dateObj = {1:{},2:{},3:{},4:{},5:{},6:{},7:{},8:{},9:{},10:{},11:{},12:{}};
     monthArr = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
     slctStatus = false;
@@ -23,6 +23,10 @@ Template.calendar_template.onCreated(function(){
 Template.calendar_template.helpers({
 
     'calendArray' : function(){
+
+        // the calendar is a grid filled with numerical values (month days number)
+        // the "calendArray" helper fill an array with 
+        // the values nessary for the grid of the calendar
 
         //console.log(`date without change : ${actual_date}`)
 
@@ -89,6 +93,7 @@ Template.calendar_template.helpers({
             return calandArray;
 
         }else{
+
             //console.log('storage got value');
             var actual_date = new Date();
             var month = actual_date.getMonth();
@@ -98,7 +103,6 @@ Template.calendar_template.helpers({
             first_day_date.setDate(1);
             var first_day = first_day_date.getDay();
             console.log(`premier jour: ${first_day}`)
-
 
             var number_of_days = new Date(year, month+1, 0).getDate();
             var number_of_days_last_week = new Date(year, month, 0).getDate();
@@ -137,6 +141,7 @@ Template.calendar_template.helpers({
         year = tmp.getFullYear()
         return year
         },
+
     //functions
 
     'isActualMonth' : function(value){
@@ -170,8 +175,8 @@ Template.calendar_template.helpers({
     },
     'setClass' : function(index, value){
         
-        /****  change class and the color value of the current day of the month 
-        * + check for existing availabilities and return class color */
+        /****  change class and the color value of the current day of the month
+        * + check for existing availabilities and return class color *********/
        
        if(Accommodation.find({host_id:Meteor.userId()}).count() === 0){
             
@@ -189,22 +194,41 @@ Template.calendar_template.helpers({
 
             //get collection data (places)
             let tmpData = Accommodation.find({host_id:Meteor.userId()}).fetch();
+
             //get every keys of the availability object document
             let tmp = Object.keys(tmpData[0].availability[actlMonth+1]);
             console.log(`new obj value : ${tmp}`)
-            dateObj = tmpData[0].availability
 
+            dateObj = tmpData[0].availability
 
             tmp.unshift('0');
             //console.log(`value : ${tmpDate.getMonth()}`)
 
             function dispDateValues(ind){
+                    console.log(`value ind : ${ind}`)
+                    console.log(`value value : ${value}`)
+
                     return ind == value;
             }
+            // grey is the class's value's day of the past or of 
+            // the futur month displayed on the calendar (lol)
+
             if(index-value <= -10){
                 return 'grey';
             }else if(index-value >= 10){
                 return 'grey';
+            
+            // change the cell's class to the "selected" class 
+            // or the "actualDay" class or the normal class "calDay"
+            
+            // the if statement uses the find() method with the "dispDate()"" 
+            // function wich is a very simple function who compares the 
+            // two numbers. The find() iterate through an array and return 
+            // the truth value of the function.
+            // the statement is used to find a date value who has been 
+            // choosed by a user and who needs to be displayed 
+            // again with the selected class
+
             }else if(tmp.find(dispDateValues)){
                     let date = new Date();
                     date.setDate(value);
@@ -212,7 +236,15 @@ Template.calendar_template.helpers({
                     dateObj[actlMonth+1][Number(value)] = dateSet;
                     return 'selected';
             }else{
+                let dt = new Date();
+                dt = dt.toDateString();
+                dt = dt.slice(8,10);
+                console.log(`ACTUAL DAY NUMBER : ${dt}`)
+                if(value == dt){
+                    return 'actualDay'
+                }else{
                     return 'calDay';
+                }
                 }
             }
     },
@@ -221,6 +253,8 @@ Template.calendar_template.helpers({
         console.log(data.userAddress);
     }
 });
+
+//calendar events
 
 Template.calendar_template.events({
     'change #monthForm': function(event){
@@ -246,7 +280,7 @@ Template.calendar_template.events({
             'year_selected' : changeYear,
             'day_selected': tmp[0].options[tmp[0].selectedIndex].value,
             });
-        //console.log(Session.get('year_selected'));
+        console.log(`day selected : ${Session.get('day_selected')}`);
     },
     'click .calDay': function(event){
         event.preventDefault();
@@ -289,25 +323,6 @@ Template.calendar_template.events({
         delete dateObj[actlMonth+1][Number(tmpValue)];
         target.className = 'calDay'
         console.log(`2) unselected result : ${JSON.stringify(dateObj)}`);
-        /*
-        else{
-
-        }
-
-        let tmpDataSlct = Accommodation.find({host_id:Meteor.userId()}).fetch();
-        dateObj = tmpDataSlct[0].availability
-        console.log(`date object : ${JSON.stringify(tmpDataSlct[0].availability)}`)
-        event.preventDefault();
-        const target = event.target;
-        let tmpValue = target.innerText;
-        console.log(dateObj[tmpValue])
-
-        if(dateObj[tmpValue]){
-            delete dateObj[Number(tmpValue)];
-            target.className = 'calDay'
-            console.log(`2) unselected result : ${JSON.stringify(dateObj)}`);
-        }
-        */
     }
 })
 
--- imports/ui/email.js
+++ imports/ui/email.js
@@ -7,7 +7,7 @@ Template.emailTemplate.events({
     'click #mail': function(event){
         Meteor.call(
             'sendEmail',
-            'localhost',
+            'renatojour@gmail.com',
             'noreply@rest.com',
             'Vous avez reçu une demande d accueil sur votre compte !',
             'clickez sur le liens pour voir la réservation'
